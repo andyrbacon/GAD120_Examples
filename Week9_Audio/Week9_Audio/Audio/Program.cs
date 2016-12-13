ac3843f54449651e50fb340fbc89548511e77266
AIV\abacon <bacon.andrew@gmail.com>
2016-12-12 19:32:11 -0800

    Audio week 9

---
+++ Week9_Audio/Week9_Audio/Audio/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Media;
+
+namespace Week9_Audio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // SETUP:
+            // 1. Drag the .wav file from Windows explorer into Visual Studio / Solution Explorer / Project Name
+            // 2. Click on the .wav file in the Solution Explorer
+            // 3. Change 'Copy to Output Directory' to 'Copy if newer'
+            // 4. Double-click on the .wav file in the Solution Explorer to open the file
+
+            SoundPlayer soundPlayer = new SoundPlayer();
+            string coinFilePath = Environment.CurrentDirectory + Path.DirectorySeparatorChar + "Blah.wav";
+            string fireBallPath = Environment.CurrentDirectory + Path.DirectorySeparatorChar + "Fireball.wav";
+            string spell = "";
+
+            while (true)
+            {
+                Console.Clear();
+
+                if (spell == "ccf")
+                {
+                    Console.WriteLine("YOU CAST A SPELL!");
+                }
+
+                if (spell == "ffc")
+                {
+                    Console.WriteLine("YOU CAST A DIFFERENT SPELL!");
+                }
+
+                if (spell.Length >= 3)
+                {
+                    spell = "";
+                }
+
+                string input = Console.ReadKey(true).KeyChar.ToString();
+
+                if (input == "c")
+                {
+                    soundPlayer.SoundLocation = coinFilePath;
+                    soundPlayer.Play();
+                    spell += "c";
+                }
+
+                if (input == "f")
+                {
+                    soundPlayer.SoundLocation = fireBallPath;
+                    soundPlayer.Play();
+                    spell += "f";
+                }
+            }
+
+        }
+    }
+}
