9146cec4cb1478ef4ce141de4c69077d1c72403d
iambacon <andy.bacon@archiactinteractive.com>
2016-12-14 15:12:18 -0800

    colorful console

--- Week9_ColorfulConsole/Week9_ColorfulConsole/Program.cs
+++ Week9_ColorfulConsole/Week9_ColorfulConsole/Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 using System.Drawing;
 using Console = Colorful.Console;
 
+// https://github.com/tomakita/Colorful.Console
+// Go to Tools / NuGet Package Manager / Package Manager Console
+// Enter: PM> Install-Package Colorful.Console
+// Colorful.Console can only write to the console in 16 different colors (including the black that's used as the console's background, by default!) in a single console session.
+
 namespace Week9_ColorfulConsole
 {
     class Program
@@ -16,14 +21,22 @@ namespace Week9_ColorfulConsole
             int g = 255;
             int b = 250;
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine("TEXT", Color.FromArgb(255, r, g, b));
-                r -= 18;
-                b -= 9;
+                Console.WriteLine("LINE " + (i+1).ToString(), Color.FromArgb(255, r, g, b));
+                r -= 30;
+                b -= 15;
             }
 
-            Console.WriteLine("ORANGE TEXT", Color.Orange);
+            Console.WriteLine();
+            Color[] colors = new Color[] { Color.Orange, Color.Pink, Color.Purple, Color.AliceBlue, Color.MediumAquamarine, Color.LightGoldenrodYellow, Color.PaleTurquoise, Color.Violet, Color.SandyBrown };
+
+            foreach(Color c in colors)
+            {
+                Console.WriteLine(c.Name, c);
+            }
+
+            Console.ResetColor();
         }
     }
 }
