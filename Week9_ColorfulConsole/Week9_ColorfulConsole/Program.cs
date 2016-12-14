3001b7560bee3302dc30f47a0c4b24a8ba1709c4
iambacon <andy.bacon@archiactinteractive.com>
2016-12-14 14:55:33 -0800

    Week 9 Colorful Console

---
+++ Week9_ColorfulConsole/Week9_ColorfulConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace Week9_ColorfulConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = 225;
+            int g = 255;
+            int b = 250;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("TEXT", Color.FromArgb(255, r, g, b));
+                r -= 18;
+                b -= 9;
+            }
+
+            Console.WriteLine("ORANGE TEXT", Color.Orange);
+        }
+    }
+}
