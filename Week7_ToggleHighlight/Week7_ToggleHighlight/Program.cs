30e93d9e63fbb67bc44dcaf6be5f8490a91698cc
andyrbacon <bacon.andrew@gmail.com>
2017-05-15 22:22:40 -0700

    Update Program.cs
    horizontal line, random colors, alt codes

--- Week7_ToggleHighlight/Week7_ToggleHighlight/Program.cs
+++ Week7_ToggleHighlight/Week7_ToggleHighlight/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,39 +10,92 @@ namespace Week7_ToggleHighlight
     {
         static void Main(string[] args)
         {
-            bool doLoop = true;
-            bool isHighlighted = false;
-            string highlightMessage = "";
-            string message = "";
+            // use an array of booleans to display a vertical line of colored lights
+            // the player can use the arrow keys to select a light
+            // press the spacebar to toggle the selected light on and off
+            Dictionary<ConsoleKey, ConsoleColor> keyColorDictionary = new Dictionary<ConsoleKey, ConsoleColor>();
+            keyColorDictionary.Add(ConsoleKey.R, ConsoleColor.Red);
+            keyColorDictionary.Add(ConsoleKey.G, ConsoleColor.Green);
+            keyColorDictionary.Add(ConsoleKey.B, ConsoleColor.Blue);
 
-            while (doLoop)
+            int selectedLightIndex = 0;
+            Random random = new Random();
+            int numberOfLights = 7;
+            bool[] isLightOn = new bool[numberOfLights];
+            ConsoleColor[] lightColors = new ConsoleColor[numberOfLights];
+            ConsoleColor[] possibleColors = new ConsoleColor[] { ConsoleColor.Red, ConsoleColor.Yellow, ConsoleColor.Green, ConsoleColor.Magenta, ConsoleColor.Cyan };
+            
+            // randomly fill up the arrays with booleans and colors
+            for (int i = 0; i < numberOfLights; i++)
+            {
+                int randomNum = random.Next(0, 2);
+                if (randomNum == 0)
+                    isLightOn[i] = true;
+                else
+                    isLightOn[i] = false;
+
+                lightColors[i] = possibleColors[random.Next(0, possibleColors.Length)];
+            }
+
+            while (true) // the game loop
             {
                 Console.Clear();
 
-                if (isHighlighted == true)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    highlightMessage = "";
+                // display line of lights
+                for (int i = 0; i < numberOfLights; i++)
+                {                   
+                    if (isLightOn[i] == true)
+                        Console.ForegroundColor = lightColors[i];
+                    else
+                        Console.ForegroundColor = ConsoleColor.Gray;
+
+                    Console.Write("☻ ");
                 }
-                else
-                {
-                    Console.ResetColor();
-                    highlightMessage = "not ";
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                for (int i = 0; i < numberOfLights; i++)
+                {   
+                    if (i == selectedLightIndex)
+                        Console.Write("▲ ");
+                    else
+                        Console.Write("  ");
                 }
 
-                message = "This is a message is " + highlightMessage + "highlighted.";
-
-                Console.WriteLine(message);
-                Console.WriteLine("Press H to toggle the highlight.");
-
-                string input = Console.ReadKey(true).KeyChar.ToString();
-
-                if (input == "h")
+                ConsoleKey input = Console.ReadKey().Key;
+                if (input == ConsoleKey.LeftArrow)
                 {
-                    isHighlighted = !isHighlighted;
+                    // move the selection up
+                    if (selectedLightIndex > 0)
+                        selectedLightIndex--;
+                    else
+                        selectedLightIndex = numberOfLights - 1;
+                }
+
+                if (input == ConsoleKey.RightArrow)
+                {
+                    // move the selection down
+                    if (selectedLightIndex < numberOfLights - 1)
+                        selectedLightIndex++;
+                    else
+                        selectedLightIndex = 0;
+                }
+
+                if (input == ConsoleKey.Spacebar)
+                {
+                    // use the ! not operator to switch the selected light
+                    isLightOn[selectedLightIndex] = !isLightOn[selectedLightIndex];
+
+                    if (isLightOn[selectedLightIndex] == true)
+                        lightColors[selectedLightIndex] = possibleColors[random.Next(0, possibleColors.Length)];
+                }
+
+                foreach(KeyValuePair<ConsoleKey, ConsoleColor> keyValue in keyColorDictionary)
+                {
+                    if (input == keyValue.Key)
+                        lightColors[selectedLightIndex] = keyValue.Value;
                 }
             }
-
         }
     }
 }
